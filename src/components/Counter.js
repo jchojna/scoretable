6a47648d8fb329c5903c9b19c2fdec71eeaaafa2
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-15 17:38:23 +0200

    chore: change Counter functional component to stateless class component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import plus from '../assets/svg/icons.svg'
 import '../scss/Counter.scss'
 
-const Counter = () => {
-  return (
-    <div className="Counter">
-      <button className="button Counter__button Counter__button--js-minus">
-        <svg className="Counter__svg" viewBox="0 0 512 512">
-          <use href={`${plus}#minus`}></use>
-        </svg>
-      </button>
-      <p className="Counter__score">
-        14
-      </p>
-      <button className="button Counter__button Counter__button--js-plus">
-        <svg className="Counter__svg" viewBox="0 0 512 512">
-          <use href={`${plus}#plus`}></use>
-        </svg>
-      </button>
-    </div>
-  );
+class Counter extends Component {
+  
+  render() {
+    return (
+      <div className="Counter">
+        <button className="button Counter__button Counter__button--js-minus">
+          <svg className="Counter__svg" viewBox="0 0 512 512">
+            <use href={`${plus}#minus`}></use>
+          </svg>
+        </button>
+        <p className="Counter__score">
+          14
+        </p>
+        <button className="button Counter__button Counter__button--js-plus">
+          <svg className="Counter__svg" viewBox="0 0 512 512">
+            <use href={`${plus}#plus`}></use>
+          </svg>
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Counter;
