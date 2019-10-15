6ddebd8296fdfffe8947725d103714134414e536
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-15 13:16:52 +0200

    chore: set folder structure for files and clean App component

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Header from './Header';
+import '../css/App.css';
+
+class App extends Component {
+  state = {
+
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
