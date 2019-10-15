78afe7e8eed136f08108f668badde7e97260e123
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-15 16:26:39 +0200

    feat: add plus/minus buttons and update player component style

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import plus from '../assets/svg/icons.svg'
 import '../scss/Counter.scss'
 
 const Counter = () => {
   return (
     <div className="Counter">
-      Counter
+      <button className="button Counter__button Counter__button--js-minus">
+        <svg className="Counter__svg" viewBox="0 0 512 512">
+          <use href={`${plus}#minus`}></use>
+        </svg>
+      </button>
+      <p className="Counter__result">
+        14
+      </p>
+      <button className="button Counter__button Counter__button--js-plus">
+        <svg className="Counter__svg" viewBox="0 0 512 512">
+          <use href={`${plus}#plus`}></use>
+        </svg>
+      </button>
     </div>
   );
 }
