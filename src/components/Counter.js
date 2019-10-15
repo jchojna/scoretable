e75f713e4c9a27799907f8c89ce5d78d437ff4e8
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-15 17:47:04 +0200

    chore: change counter component back to functional component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,32 +1,30 @@
-import React, {Component} from 'react';
+import React from 'react';
 import plus from '../assets/svg/icons.svg'
 import '../scss/Counter.scss'
 
-class Counter extends Component {
+const Counter = (props) => {
   
-  render() {
-    return (
-      <div className="Counter">
+  return (
+    <div className="Counter">
 
-        <button className="button Counter__button Counter__button--js-minus">
-          <svg className="Counter__svg" viewBox="0 0 512 512">
-            <use href={`${plus}#minus`}></use>
-          </svg>
-        </button>
+      <button className="button Counter__button Counter__button--js-minus">
+        <svg className="Counter__svg" viewBox="0 0 512 512">
+          <use href={`${plus}#minus`}></use>
+        </svg>
+      </button>
 
-        <p className="Counter__score">
-          {this.props.score}
-        </p>
+      <p className="Counter__score">
+        {props.score}
+      </p>
 
-        <button className="button Counter__button Counter__button--js-plus">
-          <svg className="Counter__svg" viewBox="0 0 512 512">
-            <use href={`${plus}#plus`}></use>
-          </svg>
-        </button>
-        
-      </div>
-    );
-  }
+      <button className="button Counter__button Counter__button--js-plus">
+        <svg className="Counter__svg" viewBox="0 0 512 512">
+          <use href={`${plus}#plus`}></use>
+        </svg>
+      </button>
+
+    </div>
+  );
 }
 
 export default Counter;
