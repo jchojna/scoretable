205dedf81a31d1c59056cb931004faa61506c423
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-15 13:45:22 +0200

    feat: add initial Player component with empty styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Header from './Header';
+import Player from './Player';
 import '../css/App.css';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <Player />
       </div>
     );
   }
---
+++ src/components/Player.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/Player.css'
+
+const Player = () => {
+  return (
+    <div>Initial</div>
+  );
+}
+
+export default Player;
