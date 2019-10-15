75c03d554a4aa0c440a0f79bd6b489ea8ad1f9c5
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-15 15:52:18 +0200

    feat: add 'crown' svg icon for best player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import '../css/Player.css'
+import Counter from './Counter';
+import icons from '../assets/svg/icons.svg'
+import '../css/Player.css';
 
 const Player = () => {
   return (
-    <div>Initial</div>
+    <section className="Player">
+      <svg className="Player__crown" viewBox="0 0 512 512">
+        <use href={`${icons}#crown`}></use>
+      </svg>
+      <h2 className="Player__heading">
+        Kuba
+      </h2>
+      <Counter />
+    </section>
   );
 }
 
