397fda63aa90a14a938957328abe1c67d63fc616
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-16 17:21:38 +0200

    refactor(js): change Header and Stopwatch components
    change Header function to an arrow function and Stopwatch to statefull
    class component

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../scss/Stopwatch.scss';
 
-function Stopwatch() {
-  return (
-    <section className="Stopwatch">
-      <h2 className="Stopwatch__header">Stopwatch</h2>
-      <span className="Stopwatch__time">0</span>
-      <button className="Stopwatch__button Stopwatch__button--start">
-        Start
-      </button>
-      <button className="Stopwatch__button Stopwatch__button--reset">
-        Reset
-      </button>
-    </section>
-  );
+class Stopwatch extends Component {
+
+  state = {
+    
+  }
+
+  render() {
+    return (
+      <section className="Stopwatch">
+        <h2 className="Stopwatch__header">Stopwatch</h2>
+        <span className="Stopwatch__time">0</span>
+        <button className="Stopwatch__button Stopwatch__button--start">
+          Start
+        </button>
+        <button className="Stopwatch__button Stopwatch__button--reset">
+          Reset
+        </button>
+      </section>
+    );
+  }
 }
 export default Stopwatch;
