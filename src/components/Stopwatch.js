9053d4a85b7948f17b86aacac9e3c33891d19830
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-16 17:14:55 +0200

    feat: set structure of Stopwatch component and initial styles

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -3,8 +3,16 @@ import '../scss/Stopwatch.scss';
 
 function Stopwatch() {
   return (
-    <p>STOPWATCH</p>
+    <section className="Stopwatch">
+      <h2 className="Stopwatch__header">Stopwatch</h2>
+      <span className="Stopwatch__time">0</span>
+      <button className="Stopwatch__button Stopwatch__button--start">
+        Start
+      </button>
+      <button className="Stopwatch__button Stopwatch__button--reset">
+        Reset
+      </button>
+    </section>
   );
 }
-
 export default Stopwatch;
