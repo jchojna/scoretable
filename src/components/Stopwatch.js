249728682a5f1046414dfe83a4ba90fa6cc3a594
Jakub Chojna <jakub.chojna@gmail.com>
2019-10-16 18:15:54 +0200

    test(js): add test-phase functionality for Stopwatch component

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -2,20 +2,48 @@ import React, { Component } from 'react';
 import '../scss/Stopwatch.scss';
 
 class Stopwatch extends Component {
-
   state = {
-    
+    isRunning: false,
+    previousTime: 0,
+    totalTime: 0
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => this.ticking(), 500);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  handleStopwatch = () => {
+    this.setState(prevState => ({
+      isRunning: !prevState.isRunning
+    }));
+  }
+
+  ticking = () => {
+    if (this.state.isRunning) {
+      console.log('running');
+    }
   }
 
   render() {
+    const {isRunning, totalTime} = this.state;
+
     return (
       <section className="Stopwatch">
         <h2 className="Stopwatch__header">Stopwatch</h2>
-        <span className="Stopwatch__time">0</span>
-        <button className="Stopwatch__button Stopwatch__button--start">
-          Start
+        <span className="Stopwatch__time">{totalTime}</span>
+        <button
+          className="Stopwatch__button Stopwatch__button--start"
+          onClick={this.handleStopwatch}
+        >
+          {isRunning ? 'Stop' : 'Start'}
         </button>
-        <button className="Stopwatch__button Stopwatch__button--reset">
+        <button
+          className="Stopwatch__button Stopwatch__button--reset"
+        >
           Reset
         </button>
       </section>
